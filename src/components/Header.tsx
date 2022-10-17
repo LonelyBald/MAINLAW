7dd79c2ee14b10221cf1bd84c15ec72553cd65e6
LonelyBald <rumba2922@gmail.com>
2022-10-18 01:36:48 +0600

    refactored

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React from 'react'
+import scale from '../assets/img/scale.jpg'
 
 export const Header = () => {
     return  (
         <div className="header">
-            <div className="container">
-                <div >MAINLAW</div>
-                <ul className="container__menu">
+            <div className="header__container">
+                <div className="logo">
+                <img src={scale} alt="" />
+                <h1>MAINLAW</h1>
+                </div>
+                <div className="menu">
+                <ul>
                     <li>О компании</li>
                     <li>Услуги</li>
                     <li>Новости</li>
                     <li>Контакты</li>
                 </ul>
-                <div >Оставить заявку</div>
+                </div>
+                <button className="button">Оставить заявку</button>
             </div>
         </div>
     )
